556ad311bd38482f424468792a1a342991a4ff56
Jordan Booker <jordan.m.booker@gmail.com>
2024-01-06 12:30:17 -0500

    first commit

---
+++ FE/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WorkoutCard from './WorkoutCard'
+
+function App() {
+  return (
+    <>
+      <h1>Lite Lift</h1>
+      <WorkoutCard />
+    </>
+  )
+}
+
+export default App
---
+++ FE/src/WorkoutCard.tsx
@@ -0,0 +1,12 @@
+import './WorkoutCard.css'
+
+function WorkoutCard() {
+  return (
+    <>
+      <p>Workout</p>
+      
+    </>
+  )
+}
+
+export default WorkoutCard
