e0d5c8861e62f1b3e81d4ec66bed4c3844779fe7
Jordan Booker <jordanmbooker@outlook.com>
2024-01-10 21:15:05 -0500

    Finished creating sql schema

--- DB/postgresdb.sql
+++ DB/postgresdb.sql
@@ -1,66 +1,76 @@
--- need to assign NOT NULL to specific columns
--- need to add function to generate UUID on insert
--- need to add enum/table for status in group
--- need to add function to generate timestamp for all created_at
-
 CREATE TABLE "follows" (
-  "id" integer PRIMARY KEY,
-  "following_user_id" uuid,
-  "followed_user_id" uuid,
-  "created_at" timestamp
+  "id" BIGSERIAL PRIMARY KEY,
+  "following_user_id" uuid NOT NULL,
+  "followed_user_id" uuid NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
 );
 
 CREATE TABLE "users" (
-  "id" integer PRIMARY KEY,
-  "sid" uuid UNIQUE,
-  "username" varchar,
-  "role" varchar,
-  "created_at" timestamp
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "username" varchar UNIQUE NOT NULL,
+  "role_id" uuid NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
 );
+
+CREATE TABLE "roles" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "type" varchar NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
+)
 
 CREATE TABLE "workout" (
   "id" integer PRIMARY KEY,
-  "sid" uuid UNIQUE,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
   "notes" text,
-  "user_id" uuid,
+  "user_id" uuid NOT NULL,
   "group_id" uuid,
-  "created_at" timestamp
+  "date" TIMESTAMP NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
 );
 
 CREATE TABLE "set" (
-  "id" integer PRIMARY KEY,
-  "sid" uuid UNIQUE,
-  "weight" integer,
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "weight" integer NOT NULL,
   "unit_id" uuid,
-  "reps" integer,
+  "reps" integer NOT NULL,
   "order" integer,
   "workout_id" uuid,
-  "category_id" uuid,
+  "category_id" uuid NOT NULL,
   "group_id" uuid,
-  "created_at" timestamp
+  "created_at" timestamp DEFAULT current_timestamp
 );
 
 CREATE TABLE "category" (
-  "id" integer PRIMARY KEY,
-  "sid" uuid UNIQUE,
-  "type" varchar,
-  "created_at" timestamp
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "type" varchar NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
 );
 
 CREATE TABLE "unit" (
-  "id" integer PRIMARY KEY,
-  "sid" uuid UNIQUE,
-  "name" varchar,
-  "created_at" timestamp
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "name" varchar NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
 );
 
-CREATE TABLE "group" (
-  "id" integer PRIMARY KEY,
-  "sid" uuid UNIQUE,
-  "name" varchar,
+CREATE TABLE "groups" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "name" varchar NOT NULL,
   "notes" text,
-  "status" varchar,
-  "created_at" timestamp
+  "status_id" uuid NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE "status" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "sid" uuid UNIQUE DEFAULT uuid_generate_v4 (),
+  "type" varchar NOT NULL,
+  "created_at" timestamp DEFAULT current_timestamp
 );
 
 ALTER TABLE "workout" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("sid");
@@ -69,12 +79,17 @@ ALTER TABLE "set" ADD FOREIGN KEY ("workout_id") REFERENCES "workout" ("sid");
 
 ALTER TABLE "set" ADD FOREIGN KEY ("category_id") REFERENCES "category" ("sid");
 
-ALTER TABLE "set" ADD FOREIGN KEY ("group_id") REFERENCES "group" ("sid");
+ALTER TABLE "set" ADD FOREIGN KEY ("group_id") REFERENCES "groups" ("sid");
 
 ALTER TABLE "set" ADD FOREIGN KEY ("unit_id") REFERENCES "unit" ("sid");
 
-ALTER TABLE "workout" ADD FOREIGN KEY ("group_id") REFERENCES "group" ("sid");
+ALTER TABLE "groups" ADD FOREIGN KEY ("status_id") REFERENCES "status" ("sid");
+
+ALTER TABLE "workout" ADD FOREIGN KEY ("group_id") REFERENCES "groups" ("sid");
 
 ALTER TABLE "follows" ADD FOREIGN KEY ("following_user_id") REFERENCES "users" ("sid");
 
 ALTER TABLE "follows" ADD FOREIGN KEY ("followed_user_id") REFERENCES "users" ("sid");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("sid");
+
